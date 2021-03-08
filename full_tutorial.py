5efdead478e4a31032e36800c8b4d67e777bb1ef
name <raronow2@gmail.com>
2021-03-08 08:40:03 -0500

    Fixed typo in URL to sig tutorial data files

--- full_tutorial.py
+++ full_tutorial.py
@@ -61,10 +61,10 @@ rna.dropna(axis=0, inplace=True)
 """
 The following section walks through an example of generating a signature matrix from raw
 data files. The data used in this tutorial are single cell profiles from the Gene
-Expression Omnibus (GEO) repository. They are already included in this compute capsule,
-and in general are available to download on the NCBI website, as well as on from our
-GitHub page, at:
-https://github.com/ShahriyariLab/TumorDecon/tree/master/TumorDecon/data/sig_matrix_example
+Expression Omnibus (GEO) repository. They are available to download on the NCBI website,
+as well as on from our GitHub page, at:
+
+https://github.com/ShahriyariLab/TumorDecon/tree/master/TumorDecon/data/sig_matrix_tutorial
 """
 
 ################################################################################
--- sig_matrix_tutorial.py
+++ sig_matrix_tutorial.py
@@ -5,7 +5,7 @@ This tutorial walks through an example of generating a signature matrix from raw
 data files. The data used in this tutorial are single cell profiles from the Gene
 Expression Omnibus (GEO) repository. They are available to download on the NCBI
 website, as well as on from our GitHub page, at:
-https://github.com/ShahriyariLab/TumorDecon/tree/master/TumorDecon/data/sig_matrix_example
+https://github.com/ShahriyariLab/TumorDecon/tree/master/TumorDecon/data/sig_matrix_tutorial
 """
 
 import TumorDecon as td
